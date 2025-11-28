decacb8696a2a27774fd8422a5ab073798040b80
Keen Rosal <keen@ethika.com>
2025-11-27 17:44:21 -0800

    Provided acronym for SOLID

--- month02-php-oop/exercises/09_SOLID_Principles.php
+++ month02-php-oop/exercises/09_SOLID_Principles.php
@@ -3,6 +3,11 @@
  * + Identify code smells
  * + Break responsibilities into classes
  * + Apply SOLID in micro-examples
+ *   S - Single Responsibility Principle (SRP)
+ *   O - Open/Closed Principle (OCP)
+ *   L - Liskov Substitution Principle (LSP)
+ *   I - Interface Segregation Principle (ISP)
+ *   D - Dependency Inversion Principle (DIP)
  */
 
 class User
