a0f67a705c9a30d46266cf2e9f3db731b9c6ccd7
Keen Rosal <keen@ethika.com>
2025-11-27 17:06:25 -0800

    moved files

--- month02-php-oop/exercises/oop/dependency-injection/FileLogger.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class FileLogger implements Logger
-{
-    public function log(string $message): void
-    {
-        file_put_contents('app.log', $message . PHP_EOL, FILE_APPEND);
-    }
-}
